a00c1f7f55800349d9b429d5529d94364862f39a
Leonardo Dantas <leonardo1317@yahoo.com.br>
2021-02-10 09:51:20 -0300

    Configuração projeto

---
+++ src/main/java/com/projeto/academia/Projeto/Academia/ProjetoAcademiaApplication.java
@@ -0,0 +1,13 @@
+package com.projeto.academia.Projeto.Academia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoAcademiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoAcademiaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projeto/academia/Projeto/Academia/ProjetoAcademiaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.academia.Projeto.Academia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoAcademiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
