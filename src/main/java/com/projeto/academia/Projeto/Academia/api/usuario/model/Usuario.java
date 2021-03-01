b1e1512c19c15fed740c4ce6eb6b2889bd22ff65
Leonardo Dantas <leonardo1317@yahoo.com.br>
2021-03-01 08:15:35 -0300

    Inicio da implementação do Spring Security

---
+++ src/main/java/com/projeto/academia/Projeto/Academia/api/usuario/model/Usuario.java
@@ -0,0 +1,64 @@
+package com.projeto.academia.Projeto.Academia.api.usuario.model;
+
+import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+@Builder
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "usuarios")
+public class Usuario implements UserDetails {
+
+    @Id
+    private Long id;
+    private String nome;
+    private String senha;
+    private String email;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    private List<Perfil> perfis = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.perfis;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return nome;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
---
+++ src/main/java/com/projeto/academia/Projeto/Academia/config/security/SecurityConfigurations.java
@@ -0,0 +1,65 @@
+package com.projeto.academia.Projeto.Academia.config.security;
+
+import com.projeto.academia.Projeto.Academia.config.security.service.TokenService;
+import io.swagger.models.HttpMethod;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private TokenService tokenService;
+
+    @Autowired
+    private IUsuarioRepository usuarioRepository;
+
+    @Autowired
+    private AutenticacaoService autenticacaoService;
+
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(autenticacaoService).passwordEncoder(NoOpPasswordEncoder.getInstance());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST,"/auth").permitAll()
+                .antMatchers(HttpMethod.GET,"/usuario").permitAll()
+                .antMatchers(HttpMethod.GET,"/usuario/*").permitAll()
+                .antMatchers(HttpMethod.POST,"/usuario/cadastrar").permitAll()
+                .antMatchers(HttpMethod.POST,"/grupo/inserir/usuario/grupo").hasRole("ALUNO")
+                .antMatchers(HttpMethod.POST,"/grupo/cadastrar").hasRole("PROFESSOR")
+                .antMatchers(HttpMethod.GET,"/grupo").hasAnyRole("PROFESSOR","ALUNO")
+                .antMatchers(HttpMethod.GET,"/grupo/*").hasAnyRole("PROFESSOR","ALUNO")
+                .antMatchers(HttpMethod.GET,"/grupo/participantes").hasAnyRole("PROFESSOR","ALUNO")
+                .antMatchers(HttpMethod.PUT,"/grupo/fechar/*").hasRole("PROFESSOR")
+                .antMatchers(HttpMethod.POST,"/grupo/finalizar/sistema").hasRole("PROFESSOR")
+                .anyRequest().denyAll()
+                .and().cors().and().csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and().addFilterBefore(new AutenticacaoViaTokenFilter(tokenService, usuarioRepository), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
+    }
+}
