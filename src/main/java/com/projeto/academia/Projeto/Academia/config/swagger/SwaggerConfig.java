00f788241ee97831f8729f833f5042211795fe1a
Leonardo Dantas <leonardo1317@yahoo.com.br>
2021-04-30 07:27:30 -0300

    Remoção de imports

--- src/main/java/com/projeto/academia/Projeto/Academia/config/swagger/SwaggerConfig.java
+++ src/main/java/com/projeto/academia/Projeto/Academia/config/swagger/SwaggerConfig.java
@@ -1,13 +1,10 @@
 package com.projeto.academia.Projeto.Academia.config.swagger;
 
-import jdk.javadoc.doclet.Doclet;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
